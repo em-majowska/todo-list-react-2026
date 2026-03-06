a5c39fb68719c85ee4ff7d82c0ea067aa0e0fd27
Ewa Majowska <e.majowska@etik.com>
2026-03-06 22:43:27 +0400

    Add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -9,74 +9,80 @@ import { FaLightbulb, FaRegLightbulb } from "react-icons/fa";
 function App() {
   const [task, setTask] = useState("haha");
   const [list, setList] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <>
-      <Header text="Todo List" />
-      <main className="container">
-        {/* not done tasks */}
-        {list.length > 0 && (
-          <div className="tasks">
-            {list
-              .filter((el) => !el.isDone)
-              .filter((el) => el.name.includes(task))
-              .map((item, index) => {
-                return (
-                  <Item
-                    key={`${index} ${item.name}`}
-                    id={item.id}
-                    value={item.name}
-                    list={list}
-                    setList={setList}
-                    isDone={item.isDone}
-                  />
-                );
-              })}
-          </div>
-        )}
-        <form
-          className="middle"
-          onSubmit={(event) => {
-            event.preventDefault();
-            if (!task) return;
-
-            const copy = [
-              ...list,
-              { name: task, isDone: false, id: Date.now() },
-            ];
-            event.target.reset();
-            setTask("");
-            setList(copy);
-          }}>
-          <Input
-            value={task}
-            onChange={(event) => {
-              setTask(event.target.value);
-            }}
-          />
-          <Button text="Add task" />
-        </form>
-        {/* done tasks */}
-        {list.length > 0 && (
-          <div className="tasks">
-            {list
-              .filter((el) => el.isDone)
-              .map((item, index) => {
-                return (
-                  <Item
-                    key={`${index}${item.name}`}
-                    id={item.id}
-                    value={item.name}
-                    list={list}
-                    setList={setList}
-                    isDone={item.isDone}
-                  />
-                );
-              })}
-          </div>
-        )}
-      </main>
-      <Footer />
+      <div className={darkMode && "dark"}>
+        <Header
+          text="Todo List"
+          btnIcon={darkMode ? <FaRegLightbulb /> : <FaLightbulb />}
+          onClick={() => setDarkMode(!darkMode)}
+        />
+        <main className="container">
+          {/* not done tasks */}
+          {list.length > 0 && (
+            <div className="tasks">
+              {list
+                .filter((el) => !el.isDone)
+                .filter((el) => el.name.includes(task))
+                .map((item, index) => {
+                  return (
+                    <Item
+                      key={`${index} ${item.name}`}
+                      id={item.id}
+                      value={item.name}
+                      list={list}
+                      setList={setList}
+                      isDone={item.isDone}
+                    />
+                  );
+                })}
+            </div>
+          )}
+          <form
+            className="middle"
+            onSubmit={(event) => {
+              event.preventDefault();
+              if (!task) return;
+              const copy = [
+                ...list,
+                { name: task, isDone: false, id: Date.now() },
+              ];
+              event.target.reset();
+              setTask("");
+              setList(copy);
+            }}>
+            <Input
+              value={task}
+              onChange={(event) => {
+                setTask(event.target.value);
+              }}
+            />
+            <Button text="Add task" />
+          </form>
+          {/* done tasks */}
+          {list.length > 0 && (
+            <div className="tasks">
+              {list
+                .filter((el) => el.isDone)
+                .map((item, index) => {
+                  return (
+                    <Item
+                      key={`${index}${item.name}`}
+                      id={item.id}
+                      value={item.name}
+                      list={list}
+                      setList={setList}
+                      isDone={item.isDone}
+                    />
+                  );
+                })}
+            </div>
+          )}
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
